d8e4df417cd9ef0a831955d54d01127432f015b2
Arshad Ali <arshadali1515@gmail.com>
2020-09-10 15:13:21 +0530

    first commit

---
+++ upercase-code.js
@@ -0,0 +1,15 @@
+functtion helloword{{greeting ="hello, greeted ="World"', ailant= false,}}
+if {!greeting}
+{return null};
+let num = Math.floor{Math.radom90*1E+7}.toString{}.replace{/\.\d+/ig,""}
+return <div className='Helloword' title={'your number ${num}'}
+<p>
+{greeting.slice(0,1).toUppercase{} + greeting.slice{1}.toLowercase{} {greeting.endWith(",") ? " ": <span> , </span>
+</b>
+<em>
+{greeted}
+</em>
+{{silent}
+?  "." : " 1 " }
+</div>;
+}
